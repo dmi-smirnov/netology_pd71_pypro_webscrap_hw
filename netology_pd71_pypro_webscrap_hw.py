ecc34040f2e5b1b109a063ac3be1541bb9474222
Dmitriy Smirnov <git@d-sm.ru>
2023-05-07 00:21:24 +0300

    additional task completed

--- netology_pd71_pypro_webscrap_hw.py
+++ netology_pd71_pypro_webscrap_hw.py
@@ -9,7 +9,8 @@ def main():
   spb_search_area_id = 2
   vacancies_search_params = {
     'text': 'Python',
-    'area': [msk_search_area_id, spb_search_area_id]
+    'area': [msk_search_area_id, spb_search_area_id],
+    'only_with_salary': 'true'
   }
 
   html_session = HTMLSession()
@@ -23,48 +24,45 @@ def main():
 
   found_vacancies = []
   for vacancy_div in vacancies_divs:
-    vacancy_href =\
-      vacancy_div.find('.serp-item__title', first=True)\
-      .attrs['href']\
-      .split(sep='?')[0]
-    
-    vacancy_req = html_session.get(url=vacancy_href)
-
-    vacancy_descr_div =\
-      vacancy_req.html.find(selector='.vacancy-section', first=True)
-    if (vacancy_descr_div.find(containing='Django') or
-        vacancy_descr_div.find(containing='Flask')):
-      vacancy = dict()
+    salary_span =\
+      vacancy_div.find(
+        selector='span[data-qa="vacancy-serp__vacancy-compensation"]',
+        first=True
+      )
+    if salary_span and 'USD' in salary_span.text:
+      vacancy_href =\
+        vacancy_div.find('.serp-item__title', first=True)\
+        .attrs['href']\
+        .split(sep='?')[0]
       
-      vacancy['href'] = vacancy_href
+      vacancy_req = html_session.get(url=vacancy_href)
 
-      salary_span =\
-        vacancy_div.find(
-          selector='span[data-qa="vacancy-serp__vacancy-compensation"]',
-          first=True
-        )
-      if salary_span:
-        salary = salary_span.text
-        vacancy['salary'] = salary
-      else:
-        vacancy['salary'] = ''
-      
-      company_name =\
-        vacancy_div.find(
-          selector='a[data-qa="vacancy-serp__vacancy-employer"]',
-          first=True
-        ).text
-      vacancy['company_name'] = company_name
+      vacancy_descr_div =\
+        vacancy_req.html.find(selector='.vacancy-section', first=True)
+      if (vacancy_descr_div.find(containing='Django') or
+          vacancy_descr_div.find(containing='Flask')):
+        vacancy = dict()
 
-      address_div =\
-        vacancy_div.find(
-          selector='div[data-qa="vacancy-serp__vacancy-address"]',
-          first=True
-        )
-      city_name = address_div.text.split(sep=',')[0]
-      vacancy['city_name'] = city_name
+        vacancy['href'] = vacancy_href
 
-      found_vacancies.append(vacancy)
+        vacancy['salary'] = salary_span.text
+        
+        company_name =\
+          vacancy_div.find(
+            selector='a[data-qa="vacancy-serp__vacancy-employer"]',
+            first=True
+          ).text
+        vacancy['company_name'] = company_name
+
+        address_div =\
+          vacancy_div.find(
+            selector='div[data-qa="vacancy-serp__vacancy-address"]',
+            first=True
+          )
+        city_name = address_div.text.split(sep=',')[0]
+        vacancy['city_name'] = city_name
+
+        found_vacancies.append(vacancy)
   
   json_file_dir_name = os.path.dirname(__file__)
   json_file_name = 'found_vacancies.json'
